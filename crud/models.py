30ecd9abb2e35d7bc7b7d0fde334a1a05faae603
yurizzs <iristiffanyu@gmail.com>
2025-05-21 23:14:21 +0800

    finish

--- crud/models.py
+++ crud/models.py
@@ -5,24 +5,24 @@ from django.db import models
 class Genders(models.Model):
     class Meta:
         db_table = 'tbl_genders'
-        
-    gender_id = models.BigAutoField(primary_key=True, blank=False) # gender_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY
-    gender = models.CharField(max_length=55, blank=False) # gender VARCHAR(55) NOT NULL
-    created_at = models.DateTimeField(auto_now_add=True) #created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    updated_at = models.DateTimeField(auto_now=True) # updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    
+
+    gender_id = models.BigAutoField(primary_key=True, blank=False)
+    gender = models.CharField(max_length=55, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 class Users(models.Model):
     class Meta:
         db_table = 'tbl_users'
-        
-    user_id = models.BigAutoField(primary_key=True, blank=False) #user_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY
-    full_name = models.CharField(max_length=55, blank=False) # full_name VARCHAR(55) NOT NULL
-    gender = models.ForeignKey(Genders, on_delete=models.CASCADE) # ender_id BIGINT NOT NULL // FOREIGN KEY(gender_id) REFERENCES tbl_genders(gender_Id) ON DELETE CASCADE
-    birth_date = models.DateField(blank=False) #birth_date NOT NULL 
-    address = models.CharField(max_length=255, blank=False) # address VARCHAR (255) NOT NULL
-    contact_number = models.CharField(max_length=55, blank=False) # contact_number VARCHAR (55) NOT NULL
-    email = models.EmailField(max_length=55, blank=False) # email VARCHAR (55) DEFAULT NULL
-    username = models.CharField(max_length=55, blank=False, unique=True) # username VARCHAR (55) NOT NULL UNIQUE
-    password = models.CharField(max_length=225, blank=False) # password VARCHAR (255) NOT NULL
-    created_at = models.DateTimeField(auto_now_add=True) #created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    updated_at = models.DateTimeField(auto_now=True) # updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+
+    user_id = models.BigAutoField(primary_key=True, blank=False)
+    full_name = models.CharField(max_length=55, blank=False)
+    gender = models.ForeignKey(Genders, on_delete=models.CASCADE)
+    birth_date = models.DateField(blank=False)
+    address = models.CharField(max_length=255, blank=False)
+    contact_number = models.CharField(max_length=55, blank=False)
+    email = models.EmailField(max_length=55, blank=True)
+    username = models.CharField(max_length=55, blank=False, unique=True)
+    password = models.CharField(max_length=255, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
